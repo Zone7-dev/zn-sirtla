e09cc57764d000e073f3aeaeb2914a7b6901f3da
Zone7-dev <76528477+Zone7-dev@users.noreply.github.com>
2021-03-26 16:02:44 +0300

    zn-sirtla

---
+++ cl_carry.lua
@@ -0,0 +1,138 @@
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+
+local carry = {
+	InProgress = false,
+	targetSrc = -1,
+	type = "",
+	personCarrying = {
+		animDict = "missfinale_c2mcs_1",
+		anim = "fin_c2_mcs_1_camman",
+		flag = 49,
+	},
+	personCarried = {
+		animDict = "nm",
+		anim = "firemans_carry",
+		attachX = 0.27,
+		attachY = 0.15,
+		attachZ = 0.63,
+		flag = 33,
+	}
+}
+
+local function drawNativeNotification(text)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(text)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+local function GetClosestPlayer(radius)
+    local players = GetActivePlayers()
+    local closestDistance = -1
+    local closestPlayer = -1
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed)
+
+    for _,playerId in ipairs(players) do
+        local targetPed = GetPlayerPed(playerId)
+        if targetPed ~= playerPed then
+            local targetCoords = GetEntityCoords(targetPed)
+            local distance = #(targetCoords-playerCoords)
+            if closestDistance == -1 or closestDistance > distance then
+                closestPlayer = playerId
+                closestDistance = distance
+            end
+        end
+    end
+	if closestDistance ~= -1 and closestDistance <= radius then
+		return closestPlayer
+	else
+		return nil
+	end
+end
+
+local function ensureAnimDict(animDict)
+    if not HasAnimDictLoaded(animDict) then
+        RequestAnimDict(animDict)
+        while not HasAnimDictLoaded(animDict) do
+            Wait(0)
+        end        
+    end
+    return animDict
+end
+
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1)
+		if IsControlJustPressed(0, Keys["F9"]) then
+			if not carry.InProgress then
+				local closestPlayer = GetClosestPlayer(3)
+				if closestPlayer then
+					local targetSrc = GetPlayerServerId(closestPlayer)
+					if targetSrc ~= -1 then
+						carry.InProgress = true
+						carry.targetSrc = targetSrc
+						TriggerServerEvent("CarryPeople:sync",targetSrc)
+						ensureAnimDict(carry.personCarrying.animDict)
+						carry.type = "carrying"
+						exports['mythic_notify']:SendAlert('inform', 'Omuzlanıyor...')
+					else
+						exports['mythic_notify']:SendAlert('error', 'Yakınlarda Kimse Yok!')
+					end
+				else
+					exports['mythic_notify']:SendAlert('error', 'Yakınlarda Kimse Yok!')
+				end
+			else
+				carry.InProgress = false
+				ClearPedSecondaryTask(PlayerPedId())
+				DetachEntity(PlayerPedId(), true, false)
+				TriggerServerEvent("CarryPeople:stop",carry.targetSrc)
+				carry.targetSrc = 0
+			end
+		end
+	end
+end)
+
+RegisterNetEvent("CarryPeople:syncTarget")
+AddEventHandler("CarryPeople:syncTarget", function(targetSrc)
+	local targetPed = GetPlayerPed(GetPlayerFromServerId(targetSrc))
+	carry.InProgress = true
+	ensureAnimDict(carry.personCarried.animDict)
+	AttachEntityToEntity(PlayerPedId(), targetPed, 0, carry.personCarried.attachX, carry.personCarried.attachY, carry.personCarried.attachZ, 0.5, 0.5, 180, false, false, false, false, 2, false)
+	carry.type = "beingcarried"
+end)
+
+RegisterNetEvent("CarryPeople:cl_stop")
+AddEventHandler("CarryPeople:cl_stop", function()
+	carry.InProgress = false
+	ClearPedSecondaryTask(PlayerPedId())
+	DetachEntity(PlayerPedId(), true, false)
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		if carry.InProgress then
+			if carry.type == "beingcarried" then
+				if not IsEntityPlayingAnim(PlayerPedId(), carry.personCarried.animDict, carry.personCarried.anim, 3) then
+					TaskPlayAnim(PlayerPedId(), carry.personCarried.animDict, carry.personCarried.anim, 8.0, -8.0, 100000, carry.personCarried.flag, 0, false, false, false)
+				end
+			elseif carry.type == "carrying" then
+				if not IsEntityPlayingAnim(PlayerPedId(), carry.personCarrying.animDict, carry.personCarrying.anim, 3) then
+					TaskPlayAnim(PlayerPedId(), carry.personCarrying.animDict, carry.personCarrying.anim, 8.0, -8.0, 100000, carry.personCarrying.flag, 0, false, false, false)
+				end
+			end
+		end
+		Wait(0)
+	end
+end)
